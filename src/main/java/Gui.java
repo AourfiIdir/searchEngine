6e0d4e300ed60877f7a58af9bcda3f0f342047cd
AourfiIdir <idir.aourfi@se.univ-bejaia.dz>
2025-11-03 13:30:10 -0500

    prototype

---
+++ src/main/java/Gui.java
@@ -0,0 +1,173 @@
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Gui extends JFrame {
+    Map<String,String> result;
+    Statement insertStatement;
+    JPanel mainPanel;
+    JLabel search;
+    JTextField fieldOfSearch;
+    JList<String> list;
+    DefaultListModel<String> model;
+    JPanel suggestionPanel;
+    JScrollPane scroll;
+    JList<String> suggestionList;
+    public static Connection conn;
+    public Gui() throws SQLException {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("search engine");
+        setSize(500,500);
+        initializer();
+        setContentPane(mainPanel);
+        searchEngine();
+        //Connection conn = createSqlConnection();
+        //createTable(conn);
+        setVisible(true);
+    }
+    public void initializer(){
+        mainPanel = new JPanel();
+        model = new DefaultListModel<String>();
+        list = new JList<>();
+
+        suggestionPanel = new JPanel();
+        scroll =  new JScrollPane(list);
+        suggestionPanel.add(scroll);
+
+        search = new JLabel("search");
+        Dimension fieldDimension = new Dimension(200,25);
+        fieldOfSearch = new JTextField();
+        fieldOfSearch.setPreferredSize(fieldDimension);
+        mainPanel.add(search);
+
+        mainPanel.add(fieldOfSearch);
+        mainPanel.add(suggestionPanel);
+    }
+    public Connection createSqlConnection() throws SQLException {
+        String url = "jdbc:sqlite:database.db";
+        try {
+            conn = DriverManager.getConnection(url);
+            insertStatement = conn.createStatement();
+            if(conn!=null){
+                System.out.println("the connections is established");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public void createTable(Connection conn) throws SQLException {
+        String sqlCreate = "CREATE TABLE IF NOT EXISTS ipes (" +
+                "id INTEGER PRIMARY KEY ,"+
+                "ip TEXT , " +
+                "title TEXT NOT NULL" +
+                ")";
+
+String sqlInsert = "INSERT OR IGNORE INTO ipes (id,ip,title) VALUES " +
+                "(1,'93.184.216.34', 'example.com'), " +
+                "(2,'142.250.190.14', 'google.com'), " +
+                "(3,'104.16.99.52', 'cloudflare.com'), " +
+                "(4,'23.63.116.135', 'amazon.com'), " +
+                "(5,'151.101.1.69', 'github.com'), " +
+                "(6,'34.102.136.180', 'openai.com'), " +
+                "(7,'104.244.42.129', 'twitter.com'), " +
+                "(8,'172.217.16.142', 'youtube.com'), " +
+                "(9,'13.227.156.76', 'aws.amazon.com'), " +
+                "(10,'52.216.72.182', 'microsoft.com'), " +
+                "(11,'185.199.108.153', 'github.io'), " +
+                "(12,'192.0.78.13', 'wordpress.com'), " +
+                "(13,'69.63.176.13', 'facebook.com'), " +
+                "(14,'157.240.22.35', 'facebook.net'), " +
+                "(15,'23.235.33.133', 'digitalocean.com'), " +
+                "(16,'127.0.0.1', 'localhost'), " + // fixed entry
+                "(17,'198.252.206.16', 'pypi.org'), " +
+                "(18,'151.101.65.121', 'npmjs.com'), " +
+                "(19,'104.18.21.25', 'medium.com'), " +
+                "(20,'172.67.211.187', 'ubuntu.com');";
+
+        try (Statement stat = conn.createStatement()) {
+            stat.execute(sqlCreate);
+            stat.execute(sqlInsert);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Map<String,String> getResult(String str,Connection conn) throws SQLException {
+        Map<String,String> resultMap= new HashMap<>();
+
+        String request = "SELECT * FROM ipes WHERE title Like '"+str+"%'";
+        PreparedStatement prepStat = conn.prepareStatement(request);
+        ResultSet result = prepStat.executeQuery();
+        while(result.next()){
+            String ip = result.getString("ip");
+            String title = result.getString("title");
+            resultMap.put(title,ip);
+        }
+        return resultMap;
+    }
+    public void updatePanel(DefaultListModel<String> model) {
+        suggestionPanel.removeAll();              // Clear old components
+        list.setModel(model);                     // Update JList data
+        suggestionPanel.add(new JScrollPane(list)); // Add the updated list back
+        suggestionPanel.revalidate();             // Refresh layout
+        suggestionPanel.repaint();                // Redraw panel
+    }
+    public void searchEngine() throws SQLException {
+        Connection conn = createSqlConnection();
+        createTable(conn);
+        fieldOfSearch.getDocument().addDocumentListener(new DocumentListener(){
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String str = fieldOfSearch.getText();
+                try {
+                    onTextChange(str);
+                    System.out.println(str);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                String str = fieldOfSearch.getText();
+                try {
+                    onTextChange(str);
+                    System.out.println(str);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                String str = fieldOfSearch.getText();
+                try {
+                    onTextChange(str);
+                    System.out.println(str);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            public void onTextChange(String str) throws SQLException {
+                //do a request to get all the titles that starts with str (retrieve also the ip) and put them in a map
+                result = getResult(str,conn);
+                //put them in  a table
+                System.out.println(result);
+                model.clear();
+                for(String i :result.keySet()){
+                    model.addElement(i);
+                }
+                //make the table clickable for any
+                updatePanel(model);
+            }
+        });
+    }
+
+}
